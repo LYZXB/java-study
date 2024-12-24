bf47a2ac064dd64351f88135b71c3b52bc07c933
zxb <zxbjinx@163.com>
2024-12-24 11:57:51 +0800

    添加泛型工具类，并添加函数式编程常用接口使用示例

---
+++ src/part_04/functional_programming_exercise/PredicateExercise.java
@@ -0,0 +1,14 @@
+package part_04.functional_programming_exercise;
+
+import java.util.function.Predicate;
+
+public class PredicateExercise {
+    public static void main(String[] args) {
+        // 接受一个输入类型为 T 的参数，并返回一个布尔值。通常用于条件判断。
+        // 用于检查字符串是否为空
+        Predicate<String> isEmpty = str -> str == null || str.isEmpty();
+
+        System.out.println("Is 'Hello' empty? " + isEmpty.test("Hello")); // 输出: false
+        System.out.println("Is '' empty? " + isEmpty.test("")); // 输出: true
+    }
+}
